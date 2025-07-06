3c9c0c09c27b799fc0c9ed3282f441ffbb3e3206
MinKhantKyaw <minkhant050603@gmail.com>
2025-07-06 23:54:13 +0700

    Header

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { useState,useEffect } from 'react'
 
 import './About.scss'
+const abouts = [
+  {title: 'Web Development', description: "I am a good Web developer.", imgUrl:''},
+  {title: 'Web Design', description: "I am a good Web developer.", imgUrl:''},
+  {title: 'UI UX', description: "I am a good Web developer.", imgUrl:''},
+  {title: 'Web Animation', description: "I am a good Web developer.", imgUrl:''}
+]
 const About = () => {
   return (
-    <div>about</div>
+    <>
+     <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+
+           <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 
